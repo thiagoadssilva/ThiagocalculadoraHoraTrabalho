857513ddda39af0c610c748de8fbc44a87ffdc5c
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-04-11 09:57:52 -0300

    Funçao que vai validar os inputs

--- frontend/src/components/Calculate/index.js
+++ frontend/src/components/Calculate/index.js
@@ -12,6 +12,7 @@ export default () => {
   const [entrance3, setEntrance3] = useState();
   const [entrance4, setEntrance4] = useState();
   const [resultClock, setResultClock] = useState("00:00");
+  const [alert, setAlert] = useState();
 
   function catchTime1(event) {
     setEntrance1(event.target.value);
@@ -26,36 +27,57 @@ export default () => {
     setEntrance4(event.target.value);
   }
 
+  function validateClock(entrance1, entrance2, entrance3, entrance4) {
+    if (entrance1 < "07:50") {
+      return "A hora da entrada 01 não pode ser menor que 07:50";
+    }
+    if (entrance2 < entrance1) {
+      return "A hora da entrada 02, não pode ser menor que a entrada 01";
+    }
+    if (entrance3 < entrance2) {
+      return "A hora da entrada 03, não pode ser menor que a entrada 02";
+    }
+    if (entrance4 < entrance3) {
+      return "A hora da entrada 04, não pode ser menor que a entrada 03";
+    }
+  }
+
   function calculateTime() {
 
-    var ms1 = moment(entrance1, "HH:mm:ss").diff(moment(entrance2, "HH:mm:ss"));
-    var convert1 = moment.duration(ms1);
-    var hours1 = Math.floor(convert1.asHours()) + moment.utc(ms1).format(":mm:ss");
+    var alert = validateClock(entrance1, entrance2, entrance3, entrance4);
 
-    var ms2 = moment(entrance3, "HH:mm:ss").diff(moment(entrance4, "HH:mm:ss"));
-    var convert2 = moment.duration(ms2);
-    var hours2 = Math.floor(convert2.asHours()) + moment.utc(ms2).format(":mm:ss");
+    if (alert) {
+      setAlert(alert);
+    } else {
+      setAlert('');
+      
+      var ms1 = moment(entrance1, "HH:mm:ss").diff(moment(entrance2, "HH:mm:ss"));
+      var convert1 = moment.duration(ms1);
+      var hours1 = Math.floor(convert1.asHours()) + moment.utc(ms1).format(":mm:ss");
 
+      var ms2 = moment(entrance3, "HH:mm:ss").diff(moment(entrance4, "HH:mm:ss"));
+      var convert2 = moment.duration(ms2);
+      var hours2 = Math.floor(convert2.asHours()) + moment.utc(ms2).format(":mm:ss");
 
-    var hours1 = hours1.split(':');
-    var hours2 = hours2.split(':');
-    var hours = parseInt(hours1[0], 10) + parseInt(hours2[0], 10);
-    var minutes = parseInt(hours1[1], 10) + parseInt(hours2[1], 10);
+      var hours1 = hours1.split(':');
+      var hours2 = hours2.split(':');
+      var hours = parseInt(hours1[0], 10) + parseInt(hours2[0], 10);
+      var minutes = parseInt(hours1[1], 10) + parseInt(hours2[1], 10);
 
-    if (minutes >= 60) {
-      minutes -= 60; hours += 1;
+      if (minutes >= 60) {
+        minutes -= 60; hours += 1;
+      }
+
+      if (minutes == '0') {
+        minutes = minutes + '0';
+      }
+
+      var horaFinal = Math.abs(hours) + ":" + minutes;
+
+      if (horaFinal != 'NaN:NaN') {
+        setResultClock(horaFinal);
+      }
     }
-
-    if (minutes == '0') {
-      minutes = minutes + '0';
-    }
-
-    var horaFinal = Math.abs(hours) + ":" + minutes;
-
-    if(horaFinal != 'NaN:NaN'){
-      setResultClock(horaFinal);
-    }
-    
   }
 
   return (
@@ -87,6 +109,8 @@ export default () => {
             <input type="time" value={entrance4} onChange={catchTime4} />
           </div>
         </div>
+
+        <p className={styles.alert}>{alert}</p>
 
         <a className={styles.containerButtonCalculator} onClick={calculateTime}>Calcular</a>
 
